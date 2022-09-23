f37cd5a50732430eab9936193a8bbdb892a1a8b3
Mauduit-O <oum-el-khaire.mauduit@hetic.net>
2022-09-23 13:57:41 +0200

    added select in react and added endpoint in spring

--- Front/ecommerce/src/component/cardCategory/cardCategory.tsx
+++ Front/ecommerce/src/component/cardCategory/cardCategory.tsx
@@ -1,20 +1,23 @@
 import * as Styled from './StyledCardCategory'
 import Button from '../../component/Buttons/Button';
+import { NavLink } from "react-router-dom";
 // import CardCatImg from './imgCategory';
 
 
 interface titleCardProps {
   titleCard : string;
   imgCardCat: string;
+  nameBtn: string;
+  link: string;
 }
 
 export default function CardCategory(props: titleCardProps): JSX.Element {
-  const {titleCard, imgCardCat} = props;
+  const {titleCard, imgCardCat, nameBtn, link} = props;
   return (
     <Styled.ContainerCategory>
       <Styled.ContainerCard>
         <Styled.TitleCardCategory>{titleCard}</Styled.TitleCardCategory>
-        <Button text='Motos' className='btnCardCat'></Button>
+        <NavLink to={'/products/'+link+''}><Button text={nameBtn} className='btnCardCat'></Button></NavLink>
       </Styled.ContainerCard>
       <Styled.CardCategoryImg src={require('../../assets/img/'+imgCardCat+'')} ></Styled.CardCategoryImg>
     </Styled.ContainerCategory>
